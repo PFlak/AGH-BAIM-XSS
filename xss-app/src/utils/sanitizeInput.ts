b4a0195c4487836e3597d764f25ecad0c0a1e294
PFlak <piotr.andrzej.flak@gmail.com>
2024-12-02 23:03:55 +0100

    hot fix zad 1

--- xss-app/src/utils/sanitizeInput.ts
+++ xss-app/src/utils/sanitizeInput.ts
@@ -1,16 +1,12 @@
 const sanitizeInput = (input: string): string => {
-    const map: Record<string, string> = {
-        '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        '"': '&quot;',
-        "'": '&#x27;',
-        '/': '&#x2F;',
-      };
-    
-      const reg: RegExp = /[&<>"'/]/g;
-    
-      return input.replace(reg, (match: string): string => map[match]);
+
+    /* 
+    *
+    *   Implement sanitization here
+    *
+    */
+
+    return input
 }
 
 export default sanitizeInput;
